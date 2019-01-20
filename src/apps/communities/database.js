e004bfb0116a1d1a29c78949bac206ee25f6bdd7
shredz7 <shredzman@gmail.com>
2019-01-20 11:47:47 -0600

    Added clearing old posts from DB

--- src/apps/communities/database.js
+++ src/apps/communities/database.js
@@ -1,6 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 
+const newStorageLimit = 4;  // How many posts maximum to store in the 'new' category.
+                            // Posts older than this limit will be deleted in clearOldPosts() as long as they are >7 days old.
+
 const dbLocation = 'db/communities.db';
 
 if(!fs.existsSync(dbLocation)) {
@@ -13,14 +16,28 @@ if(!fs.existsSync(dbLocation)) {
 
 const db = new sqlite3.Database(dbLocation);
 
+// Removes old posts >7 days old from new in community DB, keeping newStorageLimit posts at the end.
+function clearOldPosts(community, currentBlock) {
+   const query = 'DELETE FROM ' + community + ' WHERE (SELECT max(rowid) FROM '+ community + ') - ' + newStorageLimit + ' > rowid AND block < ' + (currentBlock - 20160);
+
+   db.run(query, [], function(err) {
+     if(err) {
+       throw err;
+     }
+   });
+}
+
 module.exports = {
   post: function(community, block, author, permlink) {
-    const query = 'INSERT INTO ' + community + '(block, author, permlink) VALUES (?,?,?)';
+    // Insert value and at the same time remove all val
+    const query = 'INSERT INTO ' + community + '(block, author, permlink) VALUES (?,?,?);';
     db.run(query, [block, author, permlink], function(err){
       if(err) {
         throw err
       }
     });
+
+    clearOldPosts(community, block);
   },
 
   create: function(community) {
@@ -32,7 +49,7 @@ module.exports = {
   },
 
   getNew: function(community, limit, callback) {
-    const query = 'SELECT * FROM ' + community + ' ORDER BY block DESC LIMIT ' + limit;
+    const query = 'SELECT DISTINCT rowid, author, permlink, block FROM ' + community + ' ORDER BY block DESC LIMIT ' + limit;
 
     db.all(query, [], function(err, rows) {
       if(err) {
